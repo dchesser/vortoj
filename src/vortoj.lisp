54c3bfe1a147b1eed14fceba87fa773291590453
Dave Chesser <dchesser@ggc.edu>
2018-10-06 09:23:03 -0400

    fix: random generation truly random across executions
    Of all things that I didn't expect with Common Lisp, it's that
    `random' was quite the opposite without being properly set.
    Across executions, it kept generating the same thing. How bizarre.

--- src/vortoj.lisp
+++ src/vortoj.lisp
@@ -2,5 +2,6 @@
 
 (defun main ()
   "Generate some words"
+  (setf *random-state* (make-random-state t))
   (format t "Content-Type: text/plain~%~%")
   (format t "~A~%" (generate-word default-lang (+ 2 (random 3)))))
